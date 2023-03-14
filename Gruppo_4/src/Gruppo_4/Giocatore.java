380e844b7d76ca2f135e2e237c74c885fb6aa376
CadeiLuca <126600661+CadeiLuca@users.noreply.github.com>
2023-03-14 16:38:22 +0100

    Add files via upload

---
+++ Gruppo_4/src/Gruppo_4/Giocatore.java
@@ -0,0 +1,37 @@
+package Progetto_gruppo_4;
+
+import java.util.Scanner;
+
+public class Giocatore {
+	private static int COUNTER=1;
+	private final int id;
+	private String nickname;
+	private int numGiocatore;
+	
+	public Giocatore(String nickname, int numGiocatore) {
+		this.nickname = nickname;
+		this.numGiocatore = numGiocatore;
+		this.id= COUNTER;
+		COUNTER ++;
+	}
+	
+	public String toString() {
+		return "Il giocatore inserito è: " + nickname + ", il suo numero è il: " + numGiocatore + ", il suo Id è: " + id;
+	}
+	
+	public String insertName() {
+		Scanner scanner;
+
+		scanner = new Scanner(System.in);
+		
+		
+		
+		scanner.close();
+		
+		return "";
+		
+
+	}
+	
+	
+}
